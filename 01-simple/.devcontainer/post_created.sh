fb7c829645dd76f36bdf42436ffd15dbca6b723e
shimada <curryside.net@gmail.com>
2025-04-16 22:45:46 +0900

    add compose

---
+++ 01-simple/.devcontainer/post_created.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+apt-get update && apt-get install -y vim git
+
+# tools
+go install -v golang.org/x/tools/gopls@latest
+go install -v github.com/go-delve/delve/cmd/dlv@latest
+go install -v honnef.co/go/tools/cmd/staticcheck@latest
+echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
+echo export PATH="$PATH:$(go env GOPATH)/bin" >> ~/.bashrc
+git config --local core.editor vim
+git config --local pull.rebase false
+
+# # protoc compiler
+# apt-get install -y unzip
+# cd /tmp
+# curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v26.1/protoc-26.1-linux-aarch_64.zip
+# unzip protoc-26.1-linux-aarch_64.zip -d /usr/local
+# rm -rf /tmp/bin /tmp/include /tmp/protoc-26.1-linux-aarch_64.zip
+
+# # buf cli
+# BIN="/usr/local/bin" && \
+# VERSION="1.35.1" && \
+# curl -sSL \
+# "https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf-$(uname -s)-$(uname -m)" \
+# -o "${BIN}/buf" && \
+# chmod +x "${BIN}/buf"
+
+# # protoc-gen-go
+# cd /server
+# go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+# go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+# echo export PATH="$PATH:/usr/local/protobuf/bin" >> ~/.bashrc
+
+# # grpcurl
+# go install -v github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
